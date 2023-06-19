53d460091131592992691c561914528229143b15
mori4four <mori4four@gmail.com>
2023-06-19 18:33:49 +0430

    add json-server

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,3 @@
     <router-view />
   </div>
 </template>
-
-<script setup lang="ts">
-import { useI18n } from "vue-i18n";
-const i18n = useI18n();
-console.log(i18n);
-// export default {
-//   name: "app",
-//   mounted() {
-//     this.$i18n.locale = "fa";
-//   },
-// };
-</script>
